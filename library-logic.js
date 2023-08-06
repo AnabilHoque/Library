f7d2d766cdcb25d182adad9fecebfce12dd6867a
Anabil Hoque <anabilhoque@gmail.com>
2023-08-06 21:28:01 +0100

    Initial commit

---
+++ library-logic.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
+    }
+}
+
+function run() {
+    let myLibrary = [];
+
+}
+
