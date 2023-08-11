5efa66ab22efbcb62adf2f76a7b09a52f1d9db58
Anabil Hoque <anabilhoque@gmail.com>
2023-08-11 01:47:12 +0100

    adds modal, updates header and add book sections

--- library-logic.js
+++ library-logic.js
@@ -4,13 +4,36 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
 
+    /* One way of doing of defining function
     this.info = function() {
         return `${this.title} by ${this.author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
-    }
+    }*/
 }
 
 function run() {
     let myLibrary = [];
 
+    let modal = document.querySelector("#myModal");
+    let modalBtn = document.querySelector(".add-book button");
+    let span = document.querySelector(".close");
+
+    // user clicks modal button, open modal
+    modalBtn.addEventListener("click", e => {
+        modal.style.display = "block";
+    });
+
+    // clicking on span X, close modal
+    span.addEventListener("click", e => {
+        modal.style.display = "none";
+    });
+
+    // clicking anywhere outside modal, close modal
+    window.addEventListener("click", e => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+
 }
 
+run();
