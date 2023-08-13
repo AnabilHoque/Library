c6af5283131d76163bc3b9d9b3431ac8012f0d8c
Anabil Hoque <anabilhoque@gmail.com>
2023-08-13 22:38:20 +0100

    completes library logic and style of layout

--- library-logic.js
+++ library-logic.js
@@ -35,6 +35,14 @@ Book.prototype.getRead = function() {
     return this.read;
 }
 
+Book.prototype.setPages = function(newPages) {
+    this.pages = newPages;
+}
+
+Book.prototype.setRead = function(newRead) {
+    this.read = newRead;
+}
+
 function getTableRecords() {
     const tableBody = document.querySelector("tbody");
     const numRows = tableBody.rows.length;
@@ -81,17 +89,99 @@ function removeFromMyLibrary(rowIdxInArr) {
     myLibrary.splice(rowIdxInArr, 1);
 }
 
-function addRemoveEventListeners() {
-    // adds remove event listener for existing books
+function toggleStatusFromMyLibrary(rowIdxInArr) {
+    const newRead = !myLibrary[rowIdxInArr].getRead();
+    myLibrary[rowIdxInArr].setRead(newRead);
+}
+
+function addRemoveEventListenersToExistingBooks() {
+    // adds remove event listeners for existing books
     const arrRemoveBtns = Array.from(document.querySelectorAll(".remove-button"));
-    arrRemoveBtns.forEach(btn => btn.addEventListener("click", e => {
-        const tableDataButton = e.target.parentElement;
-        const tableDataRowBook = tableDataButton.parentElement;
-        const rowIdxInArr = tableDataRowBook.rowIndex-1; // -1, as 1st table row is for the headings which is not in myLibrary
-        removeFromMyLibrary(rowIdxInArr);
-        tableDataRowBook.remove();
-    }));
-} 
+    arrRemoveBtns.forEach(btn => btn.addEventListener("click", addRemoveEventListener));
+}
+
+function addReadToggleEventListenersToExistingBooks() {
+    // adds read toggle event listeners for existing books
+    const arrStatusBtns = Array.from(document.querySelectorAll(".read-toggle"));
+    arrStatusBtns.forEach(btn => btn.addEventListener("click", addReadToggleEventListener));
+}
+
+function isBookInLibrary(book) {
+    // checks if a book object is already in the library, in which case returns its index, else returns -1
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (book.getTitle() === myLibrary[i].getTitle() && book.getAuthor() === myLibrary[i].getAuthor()) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function addRemoveEventListener(e) {
+    const tableDataButton = e.target.parentElement;
+    const tableDataRowBook = tableDataButton.parentElement;
+    const rowIdxInArr = tableDataRowBook.rowIndex-1; // -1, as 1st table row is for the headings which is not in myLibrary
+    removeFromMyLibrary(rowIdxInArr); // removes from myLibrary
+    tableDataRowBook.remove(); // removes from table
+    updateInformationSection();
+}
+
+function addReadToggleEventListener(e) {
+    const statusBtn = e.target;
+    const tableDataButton = e.target.parentElement;
+    const tableDataRowBook = tableDataButton.parentElement;
+    const rowIdxInArr = tableDataRowBook.rowIndex-1; // -1, as 1st table row is for the headings which is not in myLibrary
+    toggleStatusFromMyLibrary(rowIdxInArr);
+    // change status read -> not read and not read -> read
+    if (statusBtn.classList.contains("read-true")) {
+        statusBtn.classList.remove("read-true");
+        statusBtn.classList.add("read-false");
+        statusBtn.textContent = "Not Read";
+    } else {
+        statusBtn.classList.remove("read-false");
+        statusBtn.classList.add("read-true");
+        statusBtn.textContent = "Read";
+    }
+    updateInformationSection();
+}
+
+function addBookToTable(book) {
+    const tableBody = document.querySelector("tbody");
+    let row = tableBody.insertRow();
+    let titleCell = row.insertCell();
+    let authorCell = row.insertCell();
+    let pagesCell = row.insertCell();
+    let statusCell = row.insertCell();
+    let removeCell = row.insertCell();
+
+    titleCell.textContent = book.getTitle();
+    authorCell.textContent = book.getAuthor();
+    pagesCell.textContent = book.getPages();
+    statusCell.innerHTML = book.getRead() ? '<button type="button" class="read-toggle read-true">Read</button>' : '<button type="button" class="read-toggle read-false">Not Read</button>';
+    removeCell.innerHTML = '<button type="button" class="remove-button">Remove</button>';
+
+    // add read toggle event listener to the status read/not read button in the status cell
+    statusBtn = statusCell.children[0];
+    statusBtn.addEventListener("click", addReadToggleEventListener);
+
+    // add remove event listener to the remove button in the remove cell
+    removeBtn = removeCell.children[0];
+    removeBtn.addEventListener("click", addRemoveEventListener);
+}
+
+function updateBookInTable(book, idx) {
+    const tableBody = document.querySelector("tbody");
+    const tableBodyRows = tableBody.children;
+    const rowToChange = tableBodyRows[idx];
+    const rowToChangeCells = rowToChange.children;
+    let pagesCell = rowToChangeCells[2];
+    let statusCell = rowToChangeCells[3];
+    pagesCell.textContent = book.getPages();
+    statusCell.innerHTML = book.getRead() ? '<button type="button" class="read-toggle read-true">Read</button>' : '<button type="button" class="read-toggle read-false">Not Read</button>';
+
+    // add read toggle event listener to the status read/not read button in the status cell
+    statusBtn = statusCell.children[0];
+    statusBtn.addEventListener("click", addReadToggleEventListener);
+}
 
 function run() {
 
@@ -107,6 +197,7 @@ function run() {
 
     // clicking on span X, close modal
     closeSpan.addEventListener("click", e => {
+        modalForm.reset();
         modal.style.display = "none";
     });
 
@@ -121,19 +212,43 @@ function run() {
     modalForm.addEventListener("submit", e => {
         e.preventDefault();
         const form = e.target;
-        const allFormElems = Array.from(form.elements);
-        console.log(allFormElems[0].value);
+        const allFormElems = Array.from(form.elements); // accesses all form elements
+        const title = allFormElems[0].value;
+        const author = allFormElems[1].value;
+        const pages = allFormElems[2].value;
+        const read = allFormElems[3].checked;
+
+        const book = new Book(title, author, pages, read);
+
+        // check if the book already exists in myLibrary
+        const bookInLibraryAlready = isBookInLibrary(book);
+        if (bookInLibraryAlready === -1) {
+            // not in myLibrary already, so add
+            myLibrary.push(book);
+            addBookToTable(book);
+        } else {
+            // book is in myLibrary
+            // update existing status and number of pages read
+            myLibrary[bookInLibraryAlready].setPages(book.getPages());
+            myLibrary[bookInLibraryAlready].setRead(book.getRead());
+            updateBookInTable(book, bookInLibraryAlready);   
+        }
+
+        updateInformationSection();
         form.reset();
+        modal.style.display = "none";
     });
 
     // get existing books
     getTableRecords();
-    updateInformationSection();
 
     // add remove event listeners for existing table rows <=> existing books
-    addRemoveEventListeners();
+    addRemoveEventListenersToExistingBooks();
 
     // add read toggle event listeners for existing table rows <=> existing books
+    addReadToggleEventListenersToExistingBooks();
+
+    updateInformationSection();
 }
 
 run();
